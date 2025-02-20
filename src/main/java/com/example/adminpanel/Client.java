7f07f528f98475f0a53b86d3994727113139bd91
Даниил <elfbrus@gmail.com>
2025-02-20 20:10:16 +0300

    ренкрекр

--- src/main/java/com/example/adminpanel/Client.java
+++ src/main/java/com/example/adminpanel/Client.java
@@ -1,13 +1,48 @@
 package com.example.adminpanel;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 
 public class Client extends BorderPane {
-
     public Client() {
-        Button but1 = new Button("EduChat Admin при123");
-        setCenter(but1);
+        // Конфигурация боковой панели
+        BorderPane leftPanel = new BorderPane();
+        leftPanel.setPrefHeight(768);
+        leftPanel.setPrefWidth(400);
+        leftPanel.setStyle("-fx-background-color: #7a7a7a;");
+        //leftPanel.setPadding(new Insets(30, 0,0,12));
+
+        // Логотип
+        FlowPane logoPane = new FlowPane();
+        logoPane.setAlignment(Pos.CENTER);
+        logoPane.setPadding(new Insets(30,0,0,0));
+        Label logo = new Label();
+        logo.setMinWidth(375);
+        logo.setMinHeight(100);
+        logo.getStyleClass().add("logo");
+        logoPane.getChildren().add(logo);
+        leftPanel.setTop(logoPane);
+        setLeft(leftPanel);
+
+        // Кнопка "Выйти"
+        FlowPane exitButtonPane = new FlowPane();
+        exitButtonPane.setPrefSize(0,100);
+        exitButtonPane.setAlignment(Pos.CENTER);
+        exitButtonPane.getStyleClass().add("exitButton");
+        Label exitIcon = new Label();
+        exitIcon.getStyleClass().add("exitIcon");
+        exitIcon.setMinWidth(50);
+        exitIcon.setMinHeight(50);
+        Button exitButton = new Button("Выйти");
+        exitButton.getStyleClass().add("exitButtonText");
+        exitButtonPane.getChildren().add(exitIcon);
+        exitButtonPane.getChildren().add(exitButton);
+        leftPanel.setBottom(exitButtonPane);
     }
 
 }
