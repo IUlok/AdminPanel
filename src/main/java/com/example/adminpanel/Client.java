f1407f92ee1319c3e7a7d83052bd6f5831687e62
Даниил <elfbrus@gmail.com>
2025-02-20 20:07:13 +0300

    ренкрекр

---
+++ src/main/java/com/example/adminpanel/Client.java
@@ -0,0 +1,13 @@
+package com.example.adminpanel;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+public class Client extends BorderPane {
+
+    public Client() {
+        Button but1 = new Button("EduChat Admin при123");
+        setCenter(but1);
+    }
+
+}
--- src/main/java/com/example/adminpanel/HelloApplication.java
+++ src/main/java/com/example/adminpanel/HelloApplication.java
@@ -1,9 +1,8 @@
 package com.example.adminpanel;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.text.Font;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,9 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 768);
-        stage.setTitle("Hello!");
+        BorderPane client = new Client();
+        Scene scene = new Scene(client, 1280, 768);
+        scene.getStylesheets().add((HelloApplication.class.getResource("assets/style.css")).toString());
+        stage.setTitle("EduChat Admin");
         stage.setScene(scene);
         stage.show();
     }
