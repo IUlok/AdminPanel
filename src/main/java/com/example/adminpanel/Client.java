b6bac7bfca84afad8d45e5aff7e3c1dd31a9d577
Даниил <elfbrus@gmail.com>
2025-02-23 23:55:10 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/resources/com/example/adminpanel/assets/style.css

--- src/main/java/com/example/adminpanel/Client.java
+++ src/main/java/com/example/adminpanel/Client.java
@@ -1,18 +1,29 @@
 package com.example.adminpanel;
 
+import javafx.animation.AnimationTimer;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-import java.awt.*;
+import javax.swing.*;
+import java.time.LocalDate;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Client extends GridPane {
     private BorderPane rightPanel = new BorderPane();
     private BorderPane leftPanel = new BorderPane();
+    Stage dialogStage = new Stage();
 
     public Client() {
         // Конфигурация панели меню
@@ -164,15 +175,144 @@ public class Client extends GridPane {
         studentChoice.setAlignment(Pos.CENTER);
         studentChoice.getStyleClass().remove("radio-button");
         ToggleGroup choiceButtons = new ToggleGroup();
+        studentChoice.getStyleClass().add("studentChoice");
         prepodChoice.getStyleClass().add("prepodChoice");
-        studentChoice.getStyleClass().add("studentChoice1");
-        studentChoice.setSelected(true);
         studentChoice.setToggleGroup(choiceButtons);
         prepodChoice.setToggleGroup(choiceButtons);
         ChoicePane.getChildren().addAll(studentChoice, prepodChoice);
         ChoiceBox.getChildren().add(ChoicePane);
         rightPanel.setTop(ChoiceBox);
         add(rightPanel, 1, 0);
+
+        prepodChoice.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean isNowSelected) {
+                GridPane inputForm;
+                if(isNowSelected) {
+                    // Выбор Преподаватель: Изменение стилей
+                    studentChoice.getStyleClass().removeAll("studentChoice", "studentChoice1");
+                    prepodChoice.getStyleClass().removeAll("prepodChoice", "prepodChoice1");
+                    prepodChoice.getStyleClass().add("prepodChoice1");
+                    studentChoice.getStyleClass().add("studentChoice");
+
+                    // Форма заполнения преподавателя
+                    inputForm = new GridPane();
+                    inputForm = new GridPane();
+                    inputForm.setAlignment(Pos.CENTER);
+                    inputForm.setHgap(30);
+                    inputForm.setVgap(30);
+                    inputForm.setPadding(new Insets(25, 25, 25, 25));
+
+                    TextField firstName = new TextField();
+                    TextField lastName = new TextField();
+                    TextField thirdName = new TextField();
+                    DatePicker startingUsingAccountDate = new DatePicker();
+                    DatePicker endingUsingAccountDate = new DatePicker();
+
+                    firstName.setPromptText("Имя");
+                    lastName.setPromptText("Фамилия");
+                    thirdName.setPromptText("Отчество");
+                    startingUsingAccountDate.setValue(LocalDate.now());
+                    endingUsingAccountDate.setValue(LocalDate.now());
+
+                    ObservableList<String> department = FXCollections.observableArrayList(
+                            "Автоматика и телемеханика на железных дорогах", "Высшая математика",
+                            "Информационные и вычислительные системы", "Информатика и информационная безопасность",
+                            "Электротехника и теплоэнергетика", "Электрическая связь", "Электроснабжение железных дорог",
+                            "Архитектурно-строительное проектирование", "Водоснабжение, водоотведение и гидравлика",
+                            "Инженерная химия и естествознание", "Основания и фундаменты");
+                    ComboBox<String> departmentBox = new ComboBox<String>(department);
+
+                    ObservableList<String> position = FXCollections.observableArrayList(
+                            "Профессор", "Старший преподаватель", "Доцент", "Ассистент", "Заведующий кафедрой",
+                            "Декан", "Проректор", "Ректор");
+                    ComboBox<String> positionBox = new ComboBox<String>(position);
+
+                    ObservableList<String> degree = FXCollections.observableArrayList("Кандидат наук", "Доктор наук");
+                    ComboBox<String> degreeBox = new ComboBox<String>(degree);
+
+                    inputForm.add(firstName, 1, 1);
+                    inputForm.add(lastName, 1, 2);
+                    inputForm.add(thirdName, 1, 3);
+                    inputForm.add(startingUsingAccountDate, 1, 4);
+                    inputForm.add(endingUsingAccountDate, 1, 5);
+                    inputForm.add(departmentBox, 1, 6);
+                    inputForm.add(positionBox, 1, 7);
+                    inputForm.add(degreeBox, 1, 8);
+
+                    rightPanel.setCenter(inputForm);
+
+                    Button sendButton = new Button("Создать");
+                    FlowPane sendPane = new FlowPane(sendButton);
+                    sendPane.setAlignment(Pos.CENTER);
+                    sendPane.setPadding(new Insets(0, 0, 50, 0));
+                    rightPanel.setBottom(sendPane);
+
+                    sendButton.setOnAction(e -> {
+                        // Событие для кнопки создать
+                    });
+                }
+            }
+        });
+
+        studentChoice.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean isNowSelected) {
+                GridPane inputForm;
+                if(isNowSelected) {
+                    // Выбор студент: Изменение стилей
+                    studentChoice.getStyleClass().removeAll("studentChoice", "studentChoice1");
+                    prepodChoice.getStyleClass().removeAll("prepodChoice", "prepodChoice1");
+                    studentChoice.getStyleClass().add("studentChoice1");
+                    prepodChoice.getStyleClass().add("prepodChoice");
+
+                    // Форма заполнения студента
+                    inputForm = new GridPane();
+                    inputForm.setAlignment(Pos.CENTER);
+                    inputForm.setHgap(30);
+                    inputForm.setVgap(30);
+                    inputForm.setPadding(new Insets(25, 25, 25, 25));
+
+                    TextField firstName = new TextField();
+                    TextField lastName = new TextField();
+                    TextField thirdName = new TextField();
+                    TextField group = new TextField();
+                    DatePicker startingUsingAccountDate = new DatePicker();
+                    DatePicker endingUsingAccountDate = new DatePicker();
+
+                    firstName.setPromptText("Имя");
+                    lastName.setPromptText("Фамилия");
+                    thirdName.setPromptText("Отчество");
+                    group.setPromptText("Номер группы");
+                    startingUsingAccountDate.setValue(LocalDate.now());
+                    endingUsingAccountDate.setValue(LocalDate.now());
+
+                    ObservableList<String> compensation = FXCollections.observableArrayList("Бюджет", "Контракт", "Целевое");
+                    ComboBox<String> compensationBox = new ComboBox<String>(compensation);
+
+                    inputForm.add(firstName, 1, 1);
+                    inputForm.add(lastName, 1, 2);
+                    inputForm.add(thirdName, 1, 3);
+                    inputForm.add(group, 1, 4);
+                    inputForm.add(startingUsingAccountDate, 1, 5);
+                    inputForm.add(endingUsingAccountDate, 1, 6);
+                    inputForm.add(compensationBox, 1, 7);
+
+                    rightPanel.setCenter(inputForm);
+
+                    Button sendButton = new Button("Создать");
+                    FlowPane sendPane = new FlowPane(sendButton);
+                    sendPane.setAlignment(Pos.CENTER);
+                    sendPane.setPadding(new Insets(0, 0, 50, 0));
+                    rightPanel.setBottom(sendPane);
+
+                    sendButton.setOnAction(e -> {
+                        // Событие для кнопки создать
+                    });
+
+                }
+            }
+        });
     }
 
     protected void onUserControlButtonClick(BorderPane rightPanel) {
