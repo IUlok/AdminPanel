b41a226db26cf518196d739463a4d7365534042e
RomS2002 <romsyt2002@gmail.com>
2025-02-25 19:53:47 +0300

    Немного поработал с таблицей и всплывающей формой

--- src/main/java/com/example/adminpanel/view/GroupsPane.java
+++ src/main/java/com/example/adminpanel/view/GroupsPane.java
@@ -5,10 +5,12 @@ import com.example.adminpanel.entity.Group;
 import com.example.adminpanel.http.HttpUtil;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 import java.util.List;
 
@@ -16,9 +18,17 @@ public class GroupsPane extends VBox {
 
     private final HttpUtil httpUtil = new HttpUtil();
 
+    private final TextField groupName = new TextField("Название группы");
+    private ComboBox<String> faculty;
+    private ComboBox<String> programType;
+    private final TextField program = new TextField("Направление подготовки");
+    private ComboBox<String> studyForm;
+
     public GroupsPane() {
 
         List<Group> groupList = httpUtil.getGroups(20);
+
+        StackPane stackPane = new StackPane();
 
         ObservableList<Group> groups = FXCollections.observableArrayList(groupList);
         TableView<Group> table = new TableView<>(groups);
@@ -41,6 +51,33 @@ public class GroupsPane extends VBox {
         programColumn.setCellValueFactory(new PropertyValueFactory<>("program"));
         table.getColumns().add(programColumn);
 
-        getChildren().add(table);
+        stackPane.getChildren().add(table);
+        GridPane formPane = new GridPane();
+        formPane.setMaxSize(300, 400);
+
+        ObservableList<String> facultyList = FXCollections.observableArrayList("Автоматизация и интеллектуальные технологии",
+                "Транспортное строительство", "Управление перевозками и логистика", "Факультет безотрывных форм обучения",
+                "Промышленное и гражданское строительство", "Транспортные и энергетические системы", "Экономика и менеджмент");
+        faculty = new ComboBox<>(facultyList);
+        ObservableList<String> programTypeList = FXCollections.observableArrayList("Бакалавриат", "Специалитет",
+                "Магистратура");
+        programType = new ComboBox<>(programTypeList);
+        ObservableList<String> studyFormList = FXCollections.observableArrayList("Очная", "Очно-заочная",
+                "Заочная");
+        studyForm = new ComboBox<>(studyFormList);
+
+        formPane.add(new Text("Название группы"), 0, 0);
+        formPane.add(groupName, 1, 0);
+        formPane.add(new Text("Факультет"), 0, 1);
+        formPane.add(faculty, 1, 1);
+        formPane.add(new Text("Тип направления"), 0, 2);
+        formPane.add(programType, 1, 2);
+        formPane.add(new Text("Направление"), 0, 3);
+        formPane.add(program, 1, 3);
+        formPane.add(new Text("Форма обучения"), 0, 4);
+        formPane.add(studyForm, 1, 4);
+
+        //stackPane.getChildren().add(formPane);
+        getChildren().add(stackPane);
     }
 }
